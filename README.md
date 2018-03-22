fa73d28c7432f1cc04d3014e7e03136fbbb9bfb9
kazuhira <kazuhira.m@gmail.com>
2018-03-23 00:23:40 +0900

    support, install zenobia

---
+++ README.md
@@ -0,0 +1,6 @@
+# Zenobia
+
+## Install
+```shellscript
+$ curl https://raw.githubusercontent.com/kazuhira-r/zenobia/master/install-zenobia.sh | bash
+```
--- install-zenobia.sh
+++ install-zenobia.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 ZENOBIA_HOME=${HOME}/.zenobia
+ZENOBIA_BIN_DIR=${ZENOBIA_HOME}/bin
 
 if [  ! -e ${ZENOBIA_HOME} ]; then
     mkdir ${ZENOBIA_HOME}
 fi
+
+if [ ! -e ${ZENOBIA_BIN_DIR} ]; then
+    mkdir ${ZENOBIA_BIN_DIR}
+fi
+
+curl https://raw.githubusercontent.com/kazuhira-r/zenobia/master/bin/zenobia -o ${ZENOBIA_BIN_DIR}/zenobia
+chmod a+x ${ZENOBIA_BIN_DIR}/zenobia
