32a2138444e3ef1dfc1f88760608e15e12fed8cc
kazuhira <kazuhira.m@gmail.com>
2018-03-23 00:33:13 +0900

    install libexec scripts

--- install-zenobia.sh
+++ install-zenobia.sh
@@ -2,6 +2,7 @@
 
 ZENOBIA_HOME=${HOME}/.zenobia
 ZENOBIA_BIN_DIR=${ZENOBIA_HOME}/bin
+ZENOBIA_LIBEXEC_DIR=${ZENOBIA_HOME}/libexec
 
 if [  ! -e ${ZENOBIA_HOME} ]; then
     mkdir ${ZENOBIA_HOME}
@@ -11,5 +12,27 @@ if [ ! -e ${ZENOBIA_BIN_DIR} ]; then
     mkdir ${ZENOBIA_BIN_DIR}
 fi
 
+if [ ! -e ${ZENOBIA_LIBEXEC_DIR} ]; then
+    mkdir ${ZENOBIA_LIBEXEC_DIR}
+fi
+
 curl https://raw.githubusercontent.com/kazuhira-r/zenobia/master/bin/zenobia -o ${ZENOBIA_BIN_DIR}/zenobia
 chmod a+x ${ZENOBIA_BIN_DIR}/zenobia
+
+LIBEXEC_SCRIPTS=(
+    download.sh
+    executable.sh
+    link.sh
+    payara-micro.sh
+    wildfly-swarm.sh
+)
+
+if [ -e "${ZENOBIA_LIBEXEC_DIR}/*" ]; then
+    rm ${ZENOBIA_LIBEXEC_DIR}/*
+fi
+
+for LIBEXEC_SCRIPT in ${LIBEXEC_SCRIPT[@]}
+do
+    curl https://raw.githubusercontent.com/kazuhira-r/zenobia/master/libexec/${LIBEXEC_SCRIPT} -o ${ZENOBIA_LIBEXEC_DIR}/${LIBEXEC_SCRIPT}
+    chmod a+x ${ZENOBIA_LIBEXEC_DIR}/${LIBEXEC_SCRIPT}
+done
