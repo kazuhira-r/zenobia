bd55e4bccc188deaf7aef82187451ba8259b67f3
kazuhira <kazuhira.m@gmail.com>
2018-03-24 23:38:11 +0900

    fix, tomcat list, uninstall command

--- libexec/tomcat.sh
+++ libexec/tomcat.sh
@@ -7,18 +7,24 @@ source ${ZENOBIA_LIBEXEC_DIR}/functions.sh
 show_tomcat_version() {
     MV=$1
 
-    CURRENT_VERSION=`ls -l ${ZENOBIA_BIN_DIR}/tomcat${MV}.jar | perl -wp -e "s!.+tomcat${MV}-([^-]+).jar"'!$1!'`
+    set +e
+    COUNT=`ls -1 ${TOMCAT_DIR}/tomcat${MV}-* 2> /dev/null | grep tomcat${MV}-* | wc -l`
+    set -e
 
-    for TOMCAT_JAR in `ls -1 ${TOMCAT_DIR}/tomcat${MV}* | sort -r`
-    do
-        VERSION=`echo ${TOMCAT_JAR} | perl -wp -e "s!.+tomcat${MV}-([^-]+)\.jar"'!$1!'`
+    if [ ${COUNT} -gt 0 ]; then
+        CURRENT_VERSION=`ls -l ${ZENOBIA_BIN_DIR}/tomcat${MV}.jar | perl -wp -e "s!.+tomcat${MV}-([^-]+).jar"'!$1!'`
 
-        if [ "${CURRENT_VERSION}" == "${VERSION}" ]; then
-            echo "  ${CURRENT_VERSION} [current]"
-        else
-            echo "  ${VERSION}"
-        fi
-    done
+        for TOMCAT_JAR in `ls -1 ${TOMCAT_DIR}/tomcat${MV}-* | sort -r`
+        do
+            VERSION=`echo ${TOMCAT_JAR} | perl -wp -e "s!.+tomcat${MV}-([^-]+)\.jar"'!$1!'`
+
+            if [ "${CURRENT_VERSION}" == "${VERSION}" ]; then
+                echo "  ${CURRENT_VERSION} [current]"
+            else
+                echo "  ${VERSION}"
+            fi
+        done
+    fi
 }
 
 COMMAND=$1
@@ -122,5 +128,21 @@ elif [ "${COMMAND}" == "uninstall" ]; then
     if [ -z "${VERSION}" ]; then
         logging ERROR "require version"
         exit 1
+    fi
+
+    if [ -e ${TOMCAT_DIR}/"tomcat${MAJOR_VERSION}-${VERSION}.jar" ]; then
+        rm ${TOMCAT_DIR}/"tomcat${MAJOR_VERSION}-${VERSION}.jar"
+        logging INFO "uninstall tomcat${MAJOR_VERSION}-${VERSION}.jar"
+
+        CURRENT_VERSION=`ls -l ${ZENOBIA_BIN_DIR}/tomcat${MAJOR_VERSION}.jar | perl -wp -e "s!.+tomcat${MAJOR_VERSION}-([^-]+).jar!$1!"`
+
+        if [ "${CURRENT_VERSION}" == "${VERSION}" ]; then
+            rm ${ZENOBIA_BIN_DIR}/"tomcat${MAJOR_VERSION}.jar"
+            rm ${ZENOBIA_BIN_DIR}/"tomcat${MAJOR_VERSION}"
+            logging INFO "remove tomcat${MAJOR_VERSION}.jar"
+            logging INFO "remove tomcat${MAJOR_VERSION}"
+        fi
+    else
+        logging INFO "already uninstalled payara-micro-${VERSION}.jar"
     fi
 fi
