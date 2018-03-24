292d5ccadb7019cb104814b5204be78febe27ef1
kazuhira <kazuhira.m@gmail.com>
2018-03-24 15:02:11 +0900

    fix, typo

--- libexec/payara-micro.sh
+++ libexec/payara-micro.sh
@@ -57,17 +57,17 @@ elif [ "${COMMAND}" == "uninstall" ]; then
 
     if [ -e ${PAYARA_MICRO_DIR}/"payara-micro-${VERSION}.jar" ]; then
         rm ${PAYARA_MICRO_DIR}/"payara-micro-${VERSION}.jar"
-        logging INRO "uninstall payara-micro-${VERSION}.jar"
+        logging INFO "uninstall payara-micro-${VERSION}.jar"
 
         CURRENT_VERSION=`ls -l ${ZENOBIA_BIN_DIR}/payara-micro.jar | perl -wp -e 's!.+payara-micro-([^-]+).jar!$1!'`
 
         if [ "${CURRENT_VERSION}" == "${VERSION}" ]; then
             rm ${ZENOBIA_BIN_DIR}/"payara-micro.jar"
             rm ${ZENOBIA_BIN_DIR}/"payara-micro"
-            logging INRO "remove payara-micro.jar"
-            logging INRO "remove payara-micro"
+            logging INFO "remove payara-micro.jar"
+            logging INFO "remove payara-micro"
         fi
     else
-        logging INRO "already uninstalled payara-micro-${VERSION}.jar"
+        logging INFO "already uninstalled payara-micro-${VERSION}.jar"
     fi
 fi
