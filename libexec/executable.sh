21600832c4b21a5668caba42671aded77695ca7f
kazuhira <kazuhira.m@gmail.com>
2018-03-27 14:35:04 +0900

    support, Cygwin environment #2

--- libexec/executable.sh
+++ libexec/executable.sh
@@ -5,6 +5,7 @@ source ${ZENOBIA_LIBEXEC_DIR}/functions.sh
 TARGET_ARTIFACT_NAME=$1
 EXECUTABLE_FILE_NAME=$2
 
+if [ ${RUN_CYGWIN} -ne 1 ]; then
 JAVA_COMMAND=$(cat << 'EOS'
 java -jar "$0" "$@"
 exit $?
@@ -15,3 +16,15 @@ echo "${JAVA_COMMAND}" > ${ZENOBIA_BIN_DIR}/${EXECUTABLE_FILE_NAME}
 cat ${ZENOBIA_BIN_DIR}/${TARGET_ARTIFACT_NAME} >> ${ZENOBIA_BIN_DIR}/${EXECUTABLE_FILE_NAME}
 
 chmod a+x ${ZENOBIA_BIN_DIR}/${EXECUTABLE_FILE_NAME}
+else
+JAVA_COMMAND=$(cat << 'EOS'
+@echo off
+java -jar %~f0 %*
+exit /b
+EOS
+)
+
+echo "${JAVA_COMMAND}" | perl -wp -e 's!\n!\r\n!' > ${ZENOBIA_BIN_DIR}/${EXECUTABLE_FILE_NAME}.bat
+cat ${ZENOBIA_BIN_DIR}/${TARGET_ARTIFACT_NAME} >> ${ZENOBIA_BIN_DIR}/${EXECUTABLE_FILE_NAME}.bat
+chmod a+x ${ZENOBIA_BIN_DIR}/${EXECUTABLE_FILE_NAME}.bat
+fi
